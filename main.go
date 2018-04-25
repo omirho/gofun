ad2b060db20a71ed2140ef560b1241d1e817b052
Pulkit Arora <pulkitarora26@gmail.com>
2018-04-25 23:49:08 +0530

    open weather endpoint

--- main.go
+++ main.go
@@ -1,12 +1,54 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"strings"
+)
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/hello", Hello)
+	http.HandleFunc("/weather/", WeatherHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello!"))
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+
+func WeatherHandler(w http.ResponseWriter, r *http.Request) {
+	city := strings.SplitN(r.URL.Path, "/", 3)[2]
+
+	data, err := Query(city)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(data)
+}
+
+func Query(city string) (weatherData, error) {
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=" + os.Getenv("OPENWEATHER_API_KEY") + "&q=" + city)
+	if err != nil {
+		return weatherData{}, err
+	}
+	defer resp.Body.Close()
+
+	var weather weatherData
+
+	if err := json.NewDecoder(resp.Body).Decode(&weather); err != nil {
+		return weatherData{}, err
+	}
+
+	return weather, nil
+}
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
 }
